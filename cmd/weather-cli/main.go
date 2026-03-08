05f662ad66400be0506569b19eecffdc515f5671
OngeziweM_bets <ongeziwem@betsoftware.com>
2026-03-09 00:17:27 +0200

    Added CLI argument handling for city input

--- cmd/weather-cli/main.go
+++ cmd/weather-cli/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Go Weather CLI starting...")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run ./cmd/weather-cli \"Cape Town\"")
+		os.Exit(1)
+	}
+	city := strings.Join(os.Args[1:], " ")
+	fmt.Printf("Fetching weather for: %s\n", city)
 }
